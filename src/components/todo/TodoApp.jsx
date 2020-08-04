57bfe49b0507a290644afc6b788625bc94c63188
PBorasiya <parixit.borasia@gmail.com>
2020-08-04 15:04:55 -0700

    Started creating basic login page

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -5,6 +5,19 @@ class TodoApp extends Component{
         return (
             <div className="TodoApp">
                 My Todo App Component
+                <LoginComponent/>
+            </div>
+        )
+    }
+}
+
+class LoginComponent extends Component{
+    render(){
+        return(
+            <div className="LoginComponent">
+            UserName : <input type="text" name="username"/>
+            Password : <input type="password" name="password"/>
+            <button>Login</button>
             </div>
         )
     }
