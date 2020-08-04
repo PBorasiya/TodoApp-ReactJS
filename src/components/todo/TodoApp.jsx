88a64b8d377bb48066e5bbefb5e8de687547ad3a
PBorasiya <parixit.borasia@gmail.com>
2020-08-04 14:58:58 -0700

    Created a bare bones todo-app component

---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class TodoApp extends Component{
+    render(){
+        return (
+            <div className="TodoApp">
+                My Todo App Component
+            </div>
+        )
+    }
+}
+
+export default TodoApp
