1593eac06b9c7b4d9c97bc942c02f5445cf0cabd
PBorasiya <parixit.borasia@gmail.com>
2020-08-05 14:09:10 -0700

    prepared welcome page for connection with spring boot backend

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -3,10 +3,19 @@ import {Link} from 'react-router-dom'
 
 class WelcomeComponent  extends Component{
     render(){
-    return <div className="container">
-                <h1>Welcome {this.props.match.params.name}!!</h1>
-                You can manage your todos <Link to="/todos">here.</Link>
+    return (
+            <>
+                <h1>Welcome </h1> 
+                <div className="container">
+                    WelCome! {this.props.match.params.name}!!
+                    You can manage your todos <Link to="/todos">here.</Link>
                 </div>
+                <div className="container">
+                    Click here to get the customized welcome message. <hr/>
+                   <button className="btn btn-lg btn-info" onClick={this.showWelcomeMsg}>See customized welcome message</button>
+                </div>
+            </>
+        )
     }
 }
 
