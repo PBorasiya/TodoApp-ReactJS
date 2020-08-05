385c0c23477bb5567ba37629ca1d9ae89c0ee02f
PBorasiya <parixit.borasia@gmail.com>
2020-08-04 17:46:51 -0700

    Created a way to pass parameters through Route

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -9,7 +9,7 @@ class TodoApp extends Component{
                     <Switch>
                         <Route path="/" exact component={LoginComponent}/>
                         <Route path="/login" component={LoginComponent}/>
-                        <Route path="/welcome" component={WelcomeComponent}/>
+                        <Route path="/welcome/:name" component={WelcomeComponent}/>
                         <Route component={ErrorComponent}/>
                     </Switch>
                 </Router>
@@ -27,7 +27,7 @@ function ErrorComponent(){
 
 class WelcomeComponent  extends Component{
     render(){
-        return <div>Welcome Pranav</div>
+    return <div>Welcome {this.props.match.params.name}</div>
     }
 }
 
@@ -64,7 +64,7 @@ class LoginComponent extends Component{
 
     loginClicked(){
         if(this.state.username==='pranav' && this.state.password==='dummy'){
-            this.props.history.push("/welcome")
+            this.props.history.push(`/welcome/${this.state.username}`)
             this.setState({
                // showSuccessMessage : true,
                 //hasLoginFailed: false
