d8acc25584b5b3322e3757c6c497bc96ca8d5e43
PBorasiya <parixit.borasia@gmail.com>
2020-08-04 18:27:12 -0700

    Added extra details to todo object and used Link to redirect the Component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 
 class TodoApp extends Component{
     render(){
@@ -32,9 +32,9 @@ class ListTodosComponent  extends Component{
         this.state={
             todos : 
             [
-                {id : 1, description : 'Learn React' },
-                {id : 2, description : 'Learn GraphQl'},
-                {id : 3, description : 'Learn Node.js'},
+                {id : 1, description : 'Learn React' , done: false, targetDate : new Date()},
+                {id : 2, description : 'Learn GraphQl',done: true, targetDate : new Date()},
+                {id : 3, description : 'Learn Node.js',done: true, targetDate : new Date()},
             ]
         }
     }
@@ -46,8 +46,10 @@ class ListTodosComponent  extends Component{
                 <table>
                     <thead>
                         <tr>
-                            <th>id</th>
-                            <th>description</th>
+                            <th>Id</th>
+                            <th>Description</th>
+                            <th>Done</th>
+                            <th>Target Date</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -57,6 +59,8 @@ class ListTodosComponent  extends Component{
                                 <tr>
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toString()}</td>
                                 </tr>
                         )
                         }
@@ -70,7 +74,7 @@ class ListTodosComponent  extends Component{
 
 class WelcomeComponent  extends Component{
     render(){
-    return <div>Welcome {this.props.match.params.name}</div>
+    return <div>Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here.</Link></div>
     }
 }
 
