f481bc161ba373ea8aab8c7fcac3dab986102251
PBorasiya <parixit.borasia@gmail.com>
2020-08-04 19:27:31 -0700

    addede some more styling

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -12,6 +12,7 @@ class TodoApp extends Component{
                             <Route path="/login" component={LoginComponent}/>
                             <Route path="/welcome/:name" component={WelcomeComponent}/>
                             <Route path="/todos" component={ListTodosComponent}/>
+                            <Route path="/logout" component={LogOutComponent}/>
                             <Route component={ErrorComponent}/>
                         </Switch>
                     <FooterComponent/>
@@ -76,7 +77,10 @@ class ListTodosComponent  extends Component{
 
 class WelcomeComponent  extends Component{
     render(){
-    return <div>Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here.</Link></div>
+    return <div className="container">
+                <h1>Welcome {this.props.match.params.name}!!</h1>
+                You can manage your todos <Link to="/todos">here.</Link>
+                </div>
     }
 }
 
@@ -99,14 +103,24 @@ class LoginComponent extends Component{
 
     render(){
         return(
-            <div className="LoginComponent">
-            {/*<ShowInvalidCredential hasLoginFailed={this.state.hasLoginFailed}/>*/}
-            {this.state.hasLoginFailed && <div>Invalid Credentials!</div>}
-            {this.state.showSuccessMessage && <div>Successful Login!</div>}
-            {/*<ShowLoginSuccess showSuccessMessage={this.state.showSuccessMessage}/>*/}
-            UserName : <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
-            Password : <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
-            <button onClick={this.loginClicked}>Login</button>
+            <div>
+                <h1>Login</h1>
+                <div className="container">
+                    
+                    {this.state.hasLoginFailed && <div>Invalid Credentials!</div>}
+                    {this.state.showSuccessMessage && <div>Successful Login!</div>}
+                    <form>
+                    
+                        <div class="form-group">
+                        <label for="exampleInputEmail1">Username</label>: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/></div>
+                        
+                        <div class="form-group">
+                        <label for="exampleInputpassword">Password</label>:<input type="password" name="password" value={this.state.password} onChange={this.handleChange}/></div>
+                        
+                        <button className="btn btn-success" onClick={this.loginClicked}>Login</button>
+                    
+                    </form>
+                </div>
             </div>
         )
     }
@@ -143,12 +157,12 @@ class HeaderComponent extends Component{
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     
                         <ul className="navbar-nav">
-                            <li className="nav-link"><Link to="/welcome/pranav">Home</Link></li>
-                            <li className="nav-link"><Link to="/todos">Todos</Link></li>
+                            <li><Link to="/welcome/pranav" className="nav-link">Home</Link></li>
+                            <li><Link to="/todos" className="nav-link">Todos</Link></li>
                         </ul>
                         <ul className="navbar-nav navbar-collapse justify-content-end"> 
-                            <li className="nav-link"><Link to="/login">Login</Link></li>
-                            <li className="nav-link"><Link to="/logout">LogOut</Link></li>
+                            <li><Link to="/login" className="nav-link">Login</Link></li>
+                            <li><Link to="/logout" className="nav-link">LogOut</Link></li>
                         </ul>
                     
                 </nav>
@@ -160,8 +174,23 @@ class HeaderComponent extends Component{
 class FooterComponent extends Component{
     render(){
         return (
+            <footer className="footer">
+                <span className="text-muted">
+                    All rights reserved.
+                </span>
+            </footer>
+        )
+    }
+}
+
+class LogOutComponent extends Component{
+    render(){
+        return (
             <div>
-                Footer <hr/>
+               <h1>You are logged out.</h1>
+               <div className="Container">
+                   Thank you for using our application.
+               </div>
             </div>
         )
     }
