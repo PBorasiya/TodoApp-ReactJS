3736d23625d3465118508afaa8a1acaf4a4ff185
PBorasiya <parixit.borasia@gmail.com>
2020-08-05 20:16:32 -0700

    Created a basic form for update using Formik

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,22 +1,46 @@
 import React, {Component} from 'react'
+import moment from 'moment'
+import {Formik, Form, Field} from 'formik'
 
 class TodoComponent extends Component{
     
     constructor(props){
         super(props)
         this.state={
-            id: 4,
+            id: this.props.match.params.id,
             username : "Vishoo",
             description : "Learn forms",
             completed : false,
-            tarfetDate : new Date()
+            tarfetDate : moment(new Date()).format('YYYY-MM-DD')
         }
         
     }
     
     render(){
-        return <div>Todo Component ! {this.props.match.params.id}!</div>
-        
+        return (
+            <div>
+                <h1>TODO</h1>
+                <div className="container">
+                    <Formik>
+                        {
+                            (props) => (
+                               <Form>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description"/>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target Date</label>
+                                        <Field className="form-control" type="dare" name="targetDate"/>
+                                    </fieldset>
+                                    <button className="btn btn-success" type="submit">Submit</button>
+                               </Form>
+                            )
+                        }
+                    </Formik>
+                </div> 
+            </div>
+        )
     }
 }
 
