9c05bd0223d5cce2c0c9c0a8228392a491e0e769
PBorasiya <parixit.borasia@gmail.com>
2020-08-05 21:23:47 -0700

    Added validation for update page

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import moment from 'moment'
-import {Formik, Form, Field} from 'formik'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
 
 class TodoComponent extends Component{
     
@@ -13,23 +13,52 @@ class TodoComponent extends Component{
             completed : false,
             targetDate : moment(new Date()).format('YYYY-MM-DD')
         }
-        
+        this.onSubmit =  this.onSubmit.bind(this)
+        this.validate =  this.validate.bind(this)
     }
     
+    validate(values){
+        let errors = {}
+        if(!values.description){
+            errors.description = "Enter a descriptin"
+        }else if(values.description.length < 5){
+            errors.description = "Enter atleast 5 characters"
+        }
+
+        if(!values.targetDate){
+            errors.targetDate = "Enter date"
+        }else if(moment(values.targetDate).isValid){
+            errors.targetDate = "enter valid target date"
+        }
+        return errors
+    }
+
+    onSubmit(values){
+        console.log(values)
+    }
+  
     render(){
 
         let {description,targetDate} = this.state
-      
+
         return (
             <div>
                 <h1>TODO</h1>
                 <div className="container">
                     <Formik 
                             initialValues={{description,targetDate}}
+                            onSubmit ={this.onSubmit}
+                            validateOnBlur = {false}
+                            validateOnChange = {false}
+                            validate = {this.validate}
+                            
                     >
                         {
                             (props) => (
                                <Form>
+                                   <ErrorMessage name="description" component="div" className="alert alert-warning"/>
+                                   <ErrorMessage name="targetDate" component="div" className="alert alert-warning"/>
+
                                     <fieldset className="form-group">
                                         <label>Description</label>
                                         <Field className="form-control" type="text" name="description"/>
